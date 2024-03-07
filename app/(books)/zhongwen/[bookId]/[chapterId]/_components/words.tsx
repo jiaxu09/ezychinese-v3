49a5cbffb1fd3f3a604f60995e1065b77b274722
jiaxu09 <jiaxu99.w@gmail.com>
2024-03-07 13:56:14 +1300

    fixed

--- app/(books)/zhongwen/[bookId]/[chapterId]/_components/words.tsx
+++ app/(books)/zhongwen/[bookId]/[chapterId]/_components/words.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import WordsCard from '@/app/(books)/_components/words-card'
+import NoContent from '@/components/no-content'
 import { useGetWordsByChapter } from '@/lib/react-query/queries'
 
 import { useQuery } from '@tanstack/react-query'
@@ -23,15 +24,13 @@ const Words = ({ bookId, chapterId }: WordsProps) => {
 
   if (isFetched && data?.words.length === 0) {
     return (
-      <div className=" flex items-center justify-center ">
-        <h1 className=" text-primary">
-          Coming <span className=" text-crayola">soon</span>{' '}
-        </h1>
+      <div>
+        <NoContent />
       </div>
     )
   }
   return (
-    <div className="flex items-center justify-center space-y-4">
+    <div className='flex items-center justify-center space-y-4'>
       <WordsCard data={data} />
     </div>
   )
