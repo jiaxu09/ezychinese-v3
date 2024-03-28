8bd40ef8bce26ded7c20b40a89b427ac73992e67
jiaxu09 <jiaxu99.w@gmail.com>
2024-03-28 17:29:03 +1300

    added newline support for readings

--- app/(reading)/reading/[levelid]/[storyid]/_components/english-story.tsx
+++ app/(reading)/reading/[levelid]/[storyid]/_components/english-story.tsx
@@ -3,10 +3,18 @@ import React from 'react'
 interface EnglishStoryProps {
   story?: string
 }
+
 const EnglishStory = ({ story }: EnglishStoryProps) => {
   return (
     <div className=' leading-6 tracking-wider md:text-lg md:leading-8 md:tracking-widest'>
-      {story}
+      {story?.split('\n').map((item, key) => {
+        return (
+          <span key={key}>
+            {item}
+            <br />
+          </span>
+        )
+      })}
     </div>
   )
 }
--- app/(reading)/reading/[levelid]/[storyid]/_components/story.tsx
+++ app/(reading)/reading/[levelid]/[storyid]/_components/story.tsx
@@ -26,33 +26,37 @@ const Story = ({ story, audio }: StoryProps) => {
         )}
       </div>
       <div className=' flex w-full flex-wrap gap-1'>
-        {story?.map((item, index) => (
-          <div
-            data-tooltip-id={`${item.en}+${index}`}
-            className='flex cursor-pointer items-center justify-center  hover:border-b hover:border-dashed hover:border-gray-300'
-            key={index}
-          >
-            {item.zh.map((zh, i) => (
-              <div key={i}>
-                <ruby className='px-1'>
-                  <span className='inline-block md:text-lg'>{zh}</span>
-                  <rt className='text-sm text-gray-600 md:text-lg'>
-                    {item.pinyin[i]}
-                  </rt>
-                </ruby>
-              </div>
-            ))}
-            <Tooltip className=' max-w-xs' id={`${item.en}+${index}`}>
-              {item.hsk ? (
-                <div>
-                  {item.en} - (HSK lv.{item.hsk})
+        {story?.map((item, index) =>
+          item.zh[0] === '\n' ? (
+            <div key={index} className='w-full'></div>
+          ) : (
+            <div
+              data-tooltip-id={`${item.en}+${index}`}
+              className='flex cursor-pointer items-center justify-center  hover:border-b hover:border-dashed hover:border-gray-300'
+              key={index}
+            >
+              {item.zh.map((zh, i) => (
+                <div key={i}>
+                  <ruby className='px-1'>
+                    <span className='inline-block md:text-lg'>{zh}</span>
+                    <rt className='text-sm text-gray-600 md:text-lg'>
+                      {item.pinyin[i]}
+                    </rt>
+                  </ruby>
                 </div>
-              ) : (
-                <div>{item.en}</div>
-              )}
-            </Tooltip>
-          </div>
-        ))}
+              ))}
+              <Tooltip className=' max-w-xs' id={`${item.en}+${index}`}>
+                {item.hsk ? (
+                  <div>
+                    {item.en} - (HSK lv.{item.hsk})
+                  </div>
+                ) : (
+                  <div>{item.en}</div>
+                )}
+              </Tooltip>
+            </div>
+          )
+        )}
       </div>
     </div>
   )
